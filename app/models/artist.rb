98ef9d81d3dc1ccabb051d8a841601857c466ddb
David Zheng <davidzheng@knights.ucf.edu>
2017-07-24 12:17:27 -0400

    database and styling stuff

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,7 @@
 class Artist < ApplicationRecord
   belongs_to :user
   has_many :arts
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   def full_name
     "#{fname} #{lname}".strip
--- db/migrate/20170719150944_devise_create_users.rb
+++ db/migrate/20170719150944_devise_create_users.rb
@@ -3,6 +3,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
+      t.string :pnumber,            null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -39,6 +40,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
     end
 
     add_index :users, :email,                unique: true
+    add_index :users, :pnumber,              unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
