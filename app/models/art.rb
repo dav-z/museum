54073d849298b42d6286556e50b1a6fde7cb51f0
David Zheng <davidzheng@knights.ucf.edu>
2017-07-24 13:15:39 -0400

    making every page look the same kinda

--- app/models/art.rb
+++ app/models/art.rb
@@ -2,6 +2,6 @@ class Art < ApplicationRecord
   belongs_to :artist
   belongs_to :user
   belongs_to :location
-  has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
